a3e61bcb90182f6f789067e57ca341222c5b4fe6
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-23 17:20:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,7 @@ def line(queue)
   if queue.length==0 
     puts "The line is currently empty."
   else 
-  
-  
-  
-  
-  queue.each_with_index do |customer, index| 
+    
+    queue.each_with_index do |customer, index| 
 end
 end 
